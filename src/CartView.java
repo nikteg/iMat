22f98b4397ec7b92201f84132d82018b630a119c
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-02-27 14:43:40 +0100

    Started writing a cartview

---
+++ src/CartView.java
@@ -0,0 +1,90 @@
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+
+import se.chalmers.ait.dat215.project.ShoppingItem;
+
+import java.awt.Color;
+
+
+public class CartView extends JPanel implements PropertyChangeListener {
+
+	private IMatModel model;
+	private JPanel itemPanel;
+	private JLabel totalPriceLabel;
+	private List<ShoppingItem> itemList;
+	
+	/**
+	 * 
+	 * @param cart The cart to monitor;
+	 */
+	public CartView(IMatModel model) {
+		this.model = model;
+		itemList = new ArrayList<ShoppingItem>(model.getShoppingCart().getItems());
+		setLayout(new MigLayout("", "[126px,grow]", "[539.00,grow][][47px,fill]"));
+		
+		JPanel itemPanel = new JPanel();
+		add(itemPanel, "cell 0 0");
+		
+		JPanel summaryPanel = new JPanel();
+		add(summaryPanel, "cell 0 2,grow");
+		summaryPanel.setLayout(new MigLayout("", "[][][][][][][][][][]", "[][]"));
+		
+		JLabel totalPriceDescriptionLabel = new JLabel("Totalpris:");
+		summaryPanel.add(totalPriceDescriptionLabel, "cell 8 0");
+		
+		JLabel totalPriceLabel = new JLabel("TOTALPRIS");
+		summaryPanel.add(totalPriceLabel, "cell 9 0");
+		
+		JButton checkoutButton = new JButton("Gå till kassan");
+		summaryPanel.add(checkoutButton, "cell 9 1");
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		String command = evt.getPropertyName();
+		switch (command){
+			case "cart_add_item": updateCartView();
+			break;
+			case "cart_remove_item": updateCartView();
+			break;
+			case "cart_clear": updateCartView();
+			break;
+			case "cart_updateitem": updateCartView();
+			default: break;
+		}
+	}
+
+	private void addItem(ShoppingItem item) {
+		itemPanel.add(new CartItem(item));
+	}
+	
+	private void clearCart(){
+		itemPanel.removeAll();
+	}
+	
+	
+	
+	private void removeItem(ShoppingItem item) {
+	}
+	
+	private void updateCartView(){
+		itemList = model.getShoppingCart().getItems();
+		itemPanel.removeAll();
+		for(int i=0; i < itemList.size(); i++){
+			itemPanel.add(new CartItem(itemList.get(i)));
+		}
+		totalPriceLabel.setText("" + model.getShoppingCart().getTotal());
+	}
+}

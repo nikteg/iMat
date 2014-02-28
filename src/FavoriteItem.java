87ffb97cff6f3ad91917787fa7dabe37479d979b
Mikael Lönn <mikael.lonn@outlook.com>
2014-02-28 19:10:02 +0100

    glömde adda filer

---
+++ src/FavoriteItem.java
@@ -0,0 +1,130 @@
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JToggleButton;
+import javax.swing.ImageIcon;
+
+
+
+
+
+import javax.swing.SpinnerNumberModel;
+
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import com.alee.laf.spinner.WebSpinner;
+
+import javax.swing.JButton;
+
+import se.chalmers.ait.dat215.project.Product;
+
+import java.awt.Color;
+
+
+public class FavoriteItem extends JPanel implements ActionListener{
+	private JLabel lblNameLabel;
+	private Product product;
+	private IMatModel model;
+	private JLabel lblPriceLabel;
+	private JToggleButton tglFavorite;
+	private JButton btnAddToCart;
+
+	public FavoriteItem() {
+		super();
+		initialize();
+	}
+	
+	/**
+	 * Creates a CartItem instance from the given ShoppingItem.
+	 * @param shoppingItem - The item to track
+	 */
+	public FavoriteItem(Product product, IMatModel model){
+		this();
+		this.product = product;
+		this.model = model;
+		lblNameLabel.setText(product.getName());
+		lblPriceLabel.setText(product.getPrice() + product.getUnit());
+	}
+	
+	private void initialize() {
+		setLayout(new MigLayout("insets 4px", "[grow][64px][][pref:18.00px:pref]", "[60px]"));
+		
+		lblNameLabel = new JLabel("nameLabel");
+		add(lblNameLabel, "cell 0 0,alignx left,aligny center");
+		
+
+		
+		lblPriceLabel = new JLabel("20:-");
+		add(lblPriceLabel, "cell 1 0,alignx right,aligny center");
+		
+		btnAddToCart = new JButton("");
+		btnAddToCart.setPreferredSize(new Dimension(18, 18));
+		btnAddToCart.setUI(new javax.swing.plaf.basic.BasicButtonUI());
+		btnAddToCart.setContentAreaFilled(false);
+		btnAddToCart.setBorderPainted(false);
+		btnAddToCart.setIcon(new ImageIcon(CartItem.class.getResource("/resources/icons/cart.png")));
+		btnAddToCart.addActionListener(this);
+		btnAddToCart.setMinimumSize(new Dimension(0, 0));
+		add(btnAddToCart, "cell 2 0");
+				
+		
+		tglFavorite = new JToggleButton("");
+		tglFavorite.setUI(new javax.swing.plaf.basic.BasicButtonUI());
+		tglFavorite.setContentAreaFilled(false);
+		tglFavorite.setBorderPainted(false);
+		tglFavorite.setRolloverSelectedIcon(new ImageIcon(ItemGrid.class.getResource("/resources/icons/star.png")));
+		tglFavorite.setRolloverIcon(new ImageIcon(ItemGrid.class.getResource("/resources/icons/star-inactive.png")));
+		tglFavorite.setRolloverEnabled(true);
+		tglFavorite.setBorder(null);
+		tglFavorite.setSelectedIcon(new ImageIcon(ItemGrid.class.getResource("/resources/icons/star.png")));
+		tglFavorite.setIcon(new ImageIcon(ItemGrid.class.getResource("/resources/icons/star-outline.png")));
+		tglFavorite.addActionListener(this);
+		tglFavorite.setSelected(true);
+		tglFavorite.setActionCommand("favorite");
+		
+		add(tglFavorite, "cell 3 0,alignx right,aligny center");
+	}
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent action) {
+		if (action.getActionCommand() == "favorite") {
+			
+			if (model.isFavorite(product)) {
+				model.favoriteRemove(product);
+			} else {
+				model.favoriteAdd(product);
+			}
+ 		}
+		
+		if (action.getSource() == btnAddToCart) {
+			model.cartAddItem(product);
+		}
+		
+	}
+	
+	
+
+
+
+
+
+	
+
+	
+
+
+
+	
+
+}
---
+++ src/FavoriteView.java
@@ -0,0 +1,95 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+import net.miginfocom.swing.MigLayout;
+import se.chalmers.ait.dat215.project.ShoppingItem;
+
+
+public class FavoriteView extends JPanel implements ActionListener, PropertyChangeListener {
+
+	private IMatModel model;
+	private JButton clearFavoritesButton;
+	private JPanel favoritePanel;
+	private JScrollPane scrollPane;
+	
+	public FavoriteView() {
+		super();
+		initialize();
+	}
+	
+	public FavoriteView(IMatModel model) {
+		this.model = model;
+		this.model.addPropertyChangeListener(this);
+		initialize();
+		updateFavoriteView();
+	}
+	
+	private void initialize() {
+		setLayout(new MigLayout("insets 2px", "[grow]", "[grow][20][24]"));
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		add(scrollPane, "cell 0 0,grow");
+		
+		favoritePanel = new JPanel();
+		scrollPane.setViewportView(favoritePanel);
+		favoritePanel.setLayout(new MigLayout("insets 0px", "[grow]", "[36px]"));
+		
+		clearFavoritesButton = new JButton("Rensa favoriter");
+		add(clearFavoritesButton, "cell 0 2,alignx center");
+	}
+
+
+
+	private void updateColors() {
+		for (int i = 0; i < favoritePanel.getComponentCount(); i++) {
+			if (i % 2 == 0) {
+				favoritePanel.getComponents()[i].setBackground(Constants.ALT_COLOR);
+			} else {
+				favoritePanel.getComponents()[i].setBackground(null);
+			}
+		}
+	}
+	
+	private void updateFavoriteView() {
+		
+		favoritePanel.removeAll();
+		for (int i = 0; i < model.getFavorites().size(); i++) {
+			FavoriteItem fi = new FavoriteItem(model.getFavorites().get(i),model);
+			favoritePanel.add(fi, "wrap,growx");
+		}
+		
+		updateColors();
+		favoritePanel.revalidate();
+		repaint();
+	}
+	
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (evt.getPropertyName() == "favorite_add") {
+			updateFavoriteView();
+		}
+		
+		if (evt.getPropertyName() == "favorite_remove") {
+			updateFavoriteView();
+		}
+	}
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		
+	}
+}
